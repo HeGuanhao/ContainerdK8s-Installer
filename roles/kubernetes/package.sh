4cd54bbeef30cc0707dbe7d79f5d3222df35d34f
hgh <hgh@dameng.com>
2023-10-26 17:06:05 +0800

    add

--- roles/kubernetes/package.sh
+++ roles/kubernetes/package.sh
@@ -8,6 +8,6 @@ kubernetes_version=$(getVersion "kubernetes")
 
 yum install -y kubeadm-${kubernetes_version} kubectl-${kubernetes_version} kubelet-${kubernetes_version} ipvsadm --downloaddir ./roles/kubernetes/ --downloadonly --disableexcludes=kubernetes
 
-yum install -y *.rpm
+yum install -y ./roles/kubernetes/*.rpm
 
 kubeadm config images list > ./roles/kubernetes/images/kubernetes.txt
