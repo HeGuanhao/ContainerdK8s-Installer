f1e4b34010d32b40940ffc5298462780cf7a9c6d
hgh <hgh@dameng.com>
2023-10-26 16:25:13 +0800

    add

--- scripts/setYumRepo.sh
+++ scripts/setYumRepo.sh
@@ -13,8 +13,25 @@ enabled=1
 gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/docker-ce/linux/centos/gpg
 EOF
-
     echo "docker-ce.repo file added successfully."
 else
     echo "docker-ce.repo file already exists. No changes made."
+fi
+
+repo_file="/etc/yum.repos.d/kubernetes.repo"
+
+if [ ! -f "$repo_file" ]; then
+    cat << EOF | sudo tee "$repo_file"
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=http://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg http://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+exclude=kube*
+EOF
+    echo "kubernetes.repo file added successfully."
+else
+    echo "kubernetes.repo file already exists. No changes made."
 fi
