caadfa803cd3d6d294fb651aff1063a73f118df8
hgh <hgh@dameng.com>
2023-10-27 14:07:32 +0800

    add

--- roles/plugins/package.sh
+++ roles/plugins/package.sh
@@ -7,3 +7,5 @@ source ./scripts/utils.sh
 jq_version=$(getVersion "jq")
 
 curl -L https://github.com/jqlang/jq/releases/download/jq-1.7/jq-linux-${ARCH} -o ./roles/plugins/jq
+
+
--- scripts/imageDownload.sh
+++ scripts/imageDownload.sh
@@ -16,12 +16,13 @@ registry_version=$(getVersion "docker-registry")
 echo "pulling image: docker.io/library/registry:$registry_version"
 ./scrpits/imagePuller.sh -i "docker.io/library/registry:$registry_version"
 
-while IFS= read -r image_name
-do
-    ./scrpits/imagePuller.sh -i "$image_name"
-done < "./roles/calico/images/calico.txt"
+# while IFS= read -r image_name
+# do
+#     ./scrpits/imagePuller.sh -i "$image_name"
+# done < "./roles/calico/images/calico.txt"
 
-while IFS= read -r image_name
-do
-    ./scrpits/imagePuller.sh -i "$image_name"
-done < "./roles/kubernetes/images/kubernetes.txt"
+# while IFS= read -r image_name
+# do
+#     ./scrpits/imagePuller.sh -i "$image_name"
+# done < "./roles/kubernetes/images/kubernetes.txt"
+
